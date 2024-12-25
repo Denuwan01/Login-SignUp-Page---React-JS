5cc7e2541b8916d48d8719a698e938ee6115dd75
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-12-25 23:44:08 +0530

    Forget password button

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -26,7 +26,7 @@ const LoginSignup = () => {
                     <input type="password" />
                 </div>
             </div>
-            
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
             <div className="submit-container">
                 <div className="submit">Sign Up</div>
                 <div className="submit">Login</div>
